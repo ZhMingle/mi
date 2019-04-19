ae816968999e06f67769ed0522b9d71951eedc43
ZhMingle <1179912194@qq.com>
2019-04-19 19:48:12 +0800

    渲染category

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -8,8 +8,8 @@
     <div id="main" class="clearfix">
       <div class="left-nav">
         <ul>
-          <li v-for="item in 30" :key="item">
-            <a href="#">新品</a>
+          <li v-for="item in leftList" :key="item.category_id">
+            <a href="#">{{item.category_name}}</a>
           </li>
         </ul>
       </div>
@@ -19,12 +19,12 @@
         </a>
         <h2 class="category-title">手机</h2>
         <div class="phone-box" >
-          <div class="phone" v-for='item in 9' :key="item">
+          <div class="phone" v-for='item in leftList[0].category_list[2].body.items' :key="item.img_url">
             <a href="#">
               <img
-                src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/fd53fdc35128c8d02db7f43d2c79ab74.png?thumb=1&w=120&h=120"/>
+                :src="item.img_url"/>
             </a>
-            <h3>小米9</h3>
+            <h3>{{item.product_name}}</h3>
           </div>
         </div>
         <h2 class="category-title">电脑</h2>
@@ -44,8 +44,30 @@
 
 <script>
 </script>
+<script>
+import axios from "axios"
+
+export default {
+  data () {
+    return{
+      leftList : []
+    }
+  },
+  methods: {
+    getLeftList () {
+      axios.get('json/category1.json').then(res => {
+        this.leftList = res.data.data;
+        //  console.log(res.data.data[0].category_list[2].body.items)
+      })
+    }
+  },
+  created () {
+    this.getLeftList();
+  }
+}
+</script>
 
 <style>
-@import "http://at.alicdn.com/t/font_1008288_i7pqwosjka.css";
-@import "../styles/category.css";
+  @import "http://at.alicdn.com/t/font_1008288_i7pqwosjka.css";
+  @import "../styles/category.css";
 </style>
