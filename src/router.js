5fd18d82cedf878d77a192f6f39f6b6316a29ace
ZhMingle <1179912194@qq.com>
2019-04-19 14:10:58 +0800

    解决navbar高亮问题

--- src/router.js
+++ src/router.js
@@ -9,10 +9,10 @@ export default new VueRouter({
       path: '/',
       component: () => import('./views/HomeHasNav.vue'),
       children: [
-        { path: '/', component: () => import('./views/Home.vue') },
-        { path: 'category', component: () => import('./views/Category.vue') },
-        { path: 'cart', component: () => import('./views/Cart.vue') },
-        { path: 'user', component: () => import('./views/User.vue') }
+        { path: '/', component: () => import('./views/Home.vue'), meta: { tabNumber: 0 } },
+        { path: 'category', component: () => import('./views/Category.vue'), meta: { tabNumber: 1 } },
+        { path: 'cart', component: () => import('./views/Cart.vue'), meta: { tabNumber: 2 } },
+        { path: 'user', component: () => import('./views/User.vue'), meta: { tabNumber: 3 } }
       ]
     },
     { path: '/detail', component: () => import('./views/Detail.vue') }
